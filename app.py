064dea394c35f5eeb31f93fd902f22ba467c5491
BETH WACEKE KIMANI <kbwaceke@gmail.com>
2025-02-03 21:58:06 +0300

    added import os line

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
 import datetime
 from models import db, User, Question, Choice
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///quizzer.db')
